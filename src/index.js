0b6fd18182d31afe1db564f8f136476bd2027751
Balachinnaiah <balasagar548@gmail.com>
2026-06-24 18:06:10 +0530

    Utils usecase and Api Response and Error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv"
 import connnectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path : './.env'
 })
 
 connnectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{ console.log("Server is Up and Running on Port ", process.env.PORT)})
+    
+})
+.catch((error)=>console.log("MongoDB Connection Error in Index File!!!", error))
 
 
 
